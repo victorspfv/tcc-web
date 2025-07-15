5cd2d6d47f17e8226f20fa121817e20169e4a9a2
Victor Alvarado <vito8916@gmail.com>
2025-07-15 01:07:10 -0600

    Update menu items and site details; replace logo path and add Instagram images for enhanced content presentation.

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,16 +2,24 @@ import { IMenuItem } from "@/types/menuItems";
 
 export const menuItems: IMenuItem[] = [
     {
-        text: "Features",
-        url: "#features"
+        text: "Home",
+        url: "/"
     },
     {
-        text: "Pricing",
-        url: "#pricing"
+        text: "About Us",
+        url: "#about"
     },
     {
-        text: "Testimonials",
-        url: "#testimonials"
+        text: "Clique Crew",
+        url: "#crew"
+    },
+    {
+        text: "Philanthropy ",
+        url: "#philanthropy"
+    },
+    {
+        text: "Contact Us",
+        url: "#contact"
     }
 ];
 
@@ -24,6 +32,17 @@ export const siteDetails = {
     },
     language: 'en-us',
     locale: 'en-US',
-    siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`,
+    siteLogo: '/logo-tcc.svg',
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
 }
+
+export const instagramImages = [
+    'https://images.unsplash.com/photo-1649972904349-6e44c42644a7?w=300&h=300&fit=crop',
+    'https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?w=300&h=300&fit=crop',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=300&h=300&fit=crop',
+    'https://images.unsplash.com/photo-1721322800607-8c38375eef04?w=300&h=300&fit=crop',
+    'https://images.unsplash.com/photo-1582562124811-c09040d0a901?w=300&h=300&fit=crop',
+    'https://images.unsplash.com/photo-1649972904349-6e44c42644a7?w=300&h=300&fit=crop',
+    'https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?w=300&h=300&fit=crop',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=300&h=300&fit=crop',
+  ];
