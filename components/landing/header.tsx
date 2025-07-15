f48d16b507822131f5bbf0e7e8a322113ed0f8a9
Victor Alvarado <vito8916@gmail.com>
2025-07-14 19:58:55 -0600

    Add Header, Button, and Card components for UI structure

---
+++ components/landing/header.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import Link from 'next/link';
+import React, { useState } from 'react';
+//import { Transition } from '@headlessui/react';
+
+import { siteDetails } from '@/utils/constants';
+import { menuItems } from '@/utils/constants';
+import { Fingerprint, X, Menu} from 'lucide-react';
+
+export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <header className="bg-transparent fixed top-0 left-0 right-0 md:absolute z-50 mx-auto w-full">
+            <div className="!px-0 container">
+                <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
+                    {/* Logo */}
+                    <Link href="/" className="flex items-center gap-2">
+                        <Fingerprint className="text-foreground min-w-fit w-7 h-7" />
+                        <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
+                            {siteDetails.siteName}
+                        </span>
+                    </Link>
+
+                    {/* Desktop Menu */}
+                    <ul className="hidden md:flex space-x-6">
+                        {menuItems.map(item => (
+                            <li key={item.text}>
+                                <Link href={item.url} className="text-foreground hover:text-foreground-accent transition-colors">
+                                    {item.text}
+                                </Link>
+                            </li>
+                        ))}
+                        <li>
+                            <Link href="#cta" className="text-black bg-primary hover:bg-primary-accent px-8 py-3 rounded-full transition-colors">
+                                Download
+                            </Link>
+                        </li>
+                    </ul>
+
+                    {/* Mobile Menu Button */}
+                    <div className="md:hidden flex items-center">
+                        <button
+                            onClick={toggleMenu}
+                            type="button"
+                            className="bg-primary text-black focus:outline-none rounded-full w-10 h-10 flex items-center justify-center"
+                            aria-controls="mobile-menu"
+                            aria-expanded={isOpen}
+                        >
+                            {isOpen ? (
+                                <X className="h-6 w-6" aria-hidden="true" />
+                            ) : (
+                                <Menu className="h-6 w-6" aria-hidden="true" />
+                            )}
+                            <span className="sr-only">Toggle navigation</span>
+                        </button>
+                    </div>
+                </nav>
+            </div>
+
+            {/* Mobile Menu with Transition */}
+            <div className={`${isOpen ? 'block' : 'hidden'} transition-all duration-300 ease-in-out`}>
+                <div id="mobile-menu" className="md:hidden bg-white shadow-lg">
+                    <ul className="flex flex-col space-y-4 pt-1 pb-6 px-6">
+                        {menuItems.map(item => (
+                            <li key={item.text}>
+                                <Link href={item.url} className="text-foreground hover:text-primary block" onClick={toggleMenu}>
+                                    {item.text}
+                                </Link>
+                            </li>
+                        ))}
+                        <li>
+                            <Link href="#cta" className="text-black bg-primary hover:bg-primary-accent px-5 py-2 rounded-full block w-fit" onClick={toggleMenu}>
+                                Get Started
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+};
