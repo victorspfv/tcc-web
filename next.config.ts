5656e3bbf18f128a8ef253858dc59455b37a69cf
Victor Alvarado <vito8916@gmail.com>
2025-07-30 11:22:11 -0600

    Update Next.js configuration to remove image domains and upgrade Next.js version to 15.4.4 in package files.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ['images.unsplash.com', 'images.pexels.com'],
-  },
+  
 };
 
 export default nextConfig;
